179a7d5f21408e2ed7828fbea9933d9fb5ada317
user <gavrilik.al@gmail.com>
2023-09-01 18:31:59 +0200

    refactored interceptor configuration

--- educationapplication/src/main/java/com/innowise/educationalsystem/configuration/TenantInterceptorConfiguration.java
+++ educationapplication/src/main/java/com/innowise/educationalsystem/configuration/TenantInterceptorConfiguration.java
@@ -2,6 +2,7 @@ package com.innowise.educationalsystem.configuration;
 
 import com.innowise.educationalsystem.feign.ConnectionDetailsClient;
 import com.innowise.educationalsystem.interceptor.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,15 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Component
 public class TenantInterceptorConfiguration implements WebMvcConfigurer {
 
-    private final ConnectionDetailsClient connectionDetailsClient;
-
-    public TenantInterceptorConfiguration(ConnectionDetailsClient connectionDetailsClient) {
-        this.connectionDetailsClient = connectionDetailsClient;
-    }
+    @Autowired
+    ConnectionDetailsClient connectionDetailsClient;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestInterceptor(connectionDetailsClient)).excludePathPatterns("/api/v1/subscription/**");
-        //registry.addInterceptor(new MockRequestInterceptor()).excludePathPatterns("/api/v1/subscription/**", "/api/v1/auth/**");
     }
 }
